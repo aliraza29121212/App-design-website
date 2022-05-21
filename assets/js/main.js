19b20398996d598cc964c0d4d2f46a340e1b63ba
aliraza29121212 <95703064+aliraza29121212@users.noreply.github.com>
2022-05-21 10:47:16 -0700

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,23 @@
+// On Scroll Function
 function navMenu() {
   let navBar = document.querySelector(".navbar-sticky");
-  let scrollToButton = document.querySelector("#scrollUp");
-
+  let scrollTopButton = document.querySelector("#scrollUp");
   window.onscroll = function () {
-    var scroll = document.documentElement.scrollTo;
+    var scroll = document.documentElement.scrollTop;
     if (scroll >= 120) {
       navBar.classList.add("navbar-sticky-moved-up");
     } else {
       navBar.classList.remove("navbar-sticky-moved-up");
     }
-
-    // Apply Transition
+    // apply transition
     if (scroll >= 250) {
       navBar.classList.add("navbar-sticky-transitioned");
-      scrollTopButton.classList.add("scrollActive");
+      //scrollTopButton.classList.add("scrollActive");
     } else {
       navBar.classList.remove("navbar-sticky-transitioned");
-      scrollTopButton.classList.remove("scrollActive");
+      //scrollTopButton.classList.remove("scrollActive");
     }
-
-    //sticky on
+    // sticky on
     if (scroll >= 500) {
       navBar.classList.add("navbar-sticky-on");
     } else {
