52e37f6ed5a9d24c44565e08c0f6531a226aaeb1
aliraza29121212 <95703064+aliraza29121212@users.noreply.github.com>
2022-05-23 06:59:46 -0700

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,3 +48,28 @@ document.addEventListener("DOMContentLoaded", () => {
   counter("count3", 0, 30, 4000);
   counter("count4", 0, 10, 2000);
 });
+
+// APP-SCREEN
+var swiper = new Swiper(".swiper-container.app-screen", {
+  effect: "coverflow",
+  loop: true,
+  autoplaySpeed: 3000,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  paginationClickable: true,
+  coverflow: {
+    rotate: 0,
+    stretch: 100,
+    depth: 150,
+    modifier: 1.5,
+    slideShadows: false,
+  },
+});
